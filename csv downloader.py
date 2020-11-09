dcd6335f68fc6c83a40079fb52b68ab39582b0e1
dipeg13 <66572527+dipeg13@users.noreply.github.com>
2020-11-09 17:59:11 +0200

    Add files via upload

---
+++ csv downloader.py
@@ -0,0 +1,11 @@
+import csv
+import requests
+
+csv_url = input("Give csv's url: ")
+request = requests.get(csv_url)
+url_content = request.content
+csv_name = input("Give csv's name: ")
+csv_file = open(csv_name+".csv", 'wb')
+
+csv_file.write(url_content)
+csv_file.close()
