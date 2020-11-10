bef4730e700877818ad553bd6987a7c838ca09f8
dipeg13 <66572527+dipeg13@users.noreply.github.com>
2020-11-10 19:59:42 +0200

    Update csv downloader.py

--- csv downloader.py
+++ csv downloader.py
@@ -6,6 +6,5 @@ request = requests.get(csv_url)
 url_content = request.content
 csv_name = input("Give csv's name: ")
 csv_file = open(csv_name+".csv", 'wb')
-
 csv_file.write(url_content)
 csv_file.close()
